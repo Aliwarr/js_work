e1f8a79d18058e84e9a874375181d8f272916d2d
RAHMAT IMAM RAHBAR <91403803+Aliwarr@users.noreply.github.com>
2023-08-29 16:33:31 +0000

    first comit

---
+++ 02_moderate/Strings.js
@@ -0,0 +1,27 @@
+const name="rahmat"
+const repoCount= 50
+
+// console.log(name + repoCount);  => out dated syntax 
+
+
+
+//  string interpolation using backticks
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+// another method of declaring string
+
+const gameName = new String("rahmatFZX")
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toLocaleUpperCase());
+console.log(gameName.charAt(4));
+console.log(gameName.indexOf('t'));
+
+
+/*
+lowercase , slice , substring , splice , trim , replace , includes , split , small, blink, bold, concat, endswith, character, constructor
+*/
