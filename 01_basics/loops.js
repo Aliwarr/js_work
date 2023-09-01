62e140a53663d1482a9d9c3535db2eb2b3e32e30
RAHMAT IMAM RAHBAR <91403803+Aliwarr@users.noreply.github.com>
2023-09-01 13:50:03 +0000

    first comit

---
+++ 01_basics/loops.js
@@ -0,0 +1,34 @@
+// for loop
+for (let i = 0; i < 10 ; i++) {
+    // console.log(i);
+    
+}
+
+for (let i = 0; i < 10; i++) {
+    for(let j = 1; j < 10; j++){
+        // console.log(i + "*" +j+ "=" +i*j);
+    }
+    
+}
+
+
+// while loop
+
+let index=0
+while (index < 10) {
+    console.log(index);
+    index=index+2
+}
+
+//  do while loop 
+
+let score=11
+do {
+    console.log("score is "+ score);
+    score++;
+} while (score<=10);
+
+// break and continue can be used to control the flow of program
+
+// break : breaks the parent loop or break the control flow
+// continue : leaves that iteration or skip iteration
