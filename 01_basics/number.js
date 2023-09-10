222749de82fc3b6ad4f7e9bc8bcf561b7ef09c07
RAHMAT IMAM RAHBAR <91403803+Aliwarr@users.noreply.github.com>
2023-09-10 16:17:36 +0000

    first update

--- 01_basics/number.js
+++ 01_basics/number.js
@@ -2,14 +2,26 @@
 
 
 
-// by this method of declaring a number , we cant extract all the properties/prototypes and methods of Number data type
-const score= 400
+// by this method of declaring a number , we can not extract all the properties/prototypes and methods of Number data type
+// It doesn't have any associated properties or methods because it's a primitive value.
+let score= 400
+
+// console.log(typeof score);
 // console.log(score);
+// score.toFixed(2)
+// console.log(typeof score.toString());
+// console.log( score.toString().length);
 
 
 // This is the correct way of declaring a number , by this method we can access all the prototype of Number data type
+// It has associated properties and methods because it's an object created from the Number constructor.
+
 const balance = new Number(100)
+
+// console.log(typeof balance);
 // console.log(balance);
+// console.log(score.toFixed(2));
+
 
 // console.log(balance.toString());
 
@@ -20,7 +32,7 @@ const balance = new Number(100)
 
 /* 
 toFixed => used for decimal numbers in ecommerce   
-toprecision, toLocaleString('en-IN') , minvalue, maxvalue, maxsafeinteger, 
+toprecision, toLocaleString('en-IN') , Math.min(), Math.max(), Number.MAX_SAFE_INTEGER, 
 */
 
 
@@ -37,8 +49,11 @@ toprecision, toLocaleString('en-IN') , minvalue, maxvalue, maxsafeinteger,
 
 const min=10;
 const max=20;
-console.log(Math.floor(Math.random() * (max - min +1) ) + min);
+// console.log(Math.floor(Math.random() * (max - min +1) ) + min);
 
+let digit = 10.281
+console.log(Math.floor(digit));
+// console.log(Math.SQRT1_2);
 
 /*
 ceil, floor, sqrt, power, min, max , random
