bc0e6a84e486182e20743872bd17f2f4f6b78e6d
RAHMAT IMAM RAHBAR <91403803+Aliwarr@users.noreply.github.com>
2023-09-12 17:34:14 +0000

    first update

--- 01_basics/date.js
+++ 01_basics/date.js
@@ -1,16 +1,20 @@
 
 let mydate= new Date()
 // console.log(mydate);
-// console.log(mydate.toString());
-// console.log(mydate.toLocaleString());
 // console.log(typeof mydate);
+// console.log( mydate.toString());
+// console.log( typeof mydate.toString());
+// console.log(mydate.toLocaleString());
+// console.log(typeof mydate.toLocaleString());
+
+
 
 
 
 let myCreateddate= new Date('2023, 1, 1')
 // console.log(myCreateddate.toDateString());
 
-myCreateddate=new Date('01-14-2023')
+myCreateddate=new Date('09-08-2023')
 // console.log(myCreateddate.toLocaleDateString());
 
 
@@ -24,21 +28,42 @@ the method used is :   myCreatedDate.getTime()
 let myTimeStamp= Date.now()
 // console.log(myTimeStamp);
 // console.log(myCreateddate.getTime());
-console.log(Math.floor(Date.now()/1000));
+// console.log(Math.floor(Date.now()/1000));
 
 
 let newDate= new Date();
 
-console.log(newDate);
-console.log(newDate.getMonth());
-console.log(newDate.getDay());
+// console.log(newDate);
+// console.log(newDate.getMonth());
+// console.log(newDate.getDay());
 
 
 // interesting
 
-newDate.toLocaleString('default', {
-    weekday : "long"
-})
+// console.log(newDate.toLocaleString('default', {
+//     weekday : "long"
+// }));
 
+let testDate = new Date()
+// console.log(testDate.getTime());
+// console.log(testDate.getFullYear());
+// console.log(testDate.getMonth());
+// console.log(testDate.getDate()); 
+// console.log(testDate.getHours());
+// console.log(testDate.getMinutes());
+// console.log(testDate.getSeconds());
+// console.log(testDate.getMilliseconds());
+// console.log(testDate.setHours(15));
+//  console.log(testDate.setDate(10));
+// console.log(testDate.setMonth(4));
+// console.log(testDate.setFullYear(2022)); 
+//  console.log(testDate.getTimezoneOffset());
+// console.log(testDate.toISOString());
+//  console.log(testDate.toJSON());
+// console.log(testDate.toUTCString()) ;
+// console.log(testDate.toLocaleDateString());
+// console.log(testDate.valueOf());
+
+ 
 
 // todateString, tolocaleString, toIsoString, toJSON, toLocaleDateString, toLocaleTimeString 
